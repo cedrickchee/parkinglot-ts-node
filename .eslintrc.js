78c576a4338e0429078f3c8a645c367841bf1b9f
Cedric Chee <cedric@invictusbyte.com>
2019-12-22 05:48:12 +0800

    Customize ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,12 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["airbnb-typescript/base", "prettier/@typescript-eslint"]
+  extends: [
+    "airbnb-typescript/base",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  rules: {
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }]
+  }
 };
